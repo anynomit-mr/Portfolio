93723849b9ffdd61e428837b4edbc80b0f6d4cf2
anynomit-mr <164717523+anynomit-mr@users.noreply.github.com>
2025-03-11 03:56:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',  // Add this line to set the base path for GitHub Pages
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
   },
 })
-
-
-
-    
